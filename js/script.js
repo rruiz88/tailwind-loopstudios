4557982205dc9bf531b2c59bbc3b99b01740fbe5
rruiz88 <roman.ruiz816@gmail.com>
2023-09-01 12:19:11 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const navToggle = () => {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+};
+
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+btn.addEventListener("click", navToggle);
